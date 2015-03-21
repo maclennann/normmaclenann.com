dcca37e72e995d504ee2f6e064f87345f9ed40db
Norm MacLennan <norm.maclennan@gmail.com>
2015-03-21 15:22:45 -0400

    only run javascript onread

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,28 @@
 'use strict';
 
-function makeUL(array) {
-  var list = document.createElement('ul');
+$(function() {
+  function makeUL(array) {
+    var list = document.createElement('ul');
 
-  array.slice(0,10).forEach(function(e){
-    var item = document.createElement('li');
-    var link = document.createElement('a');
-    link.href = e.link;
-    link.appendChild(document.createTextNode(e.title));
-    item.appendChild(link);
+    array.slice(0,10).forEach(function(e){
+      var item = document.createElement('li');
+      var link = document.createElement('a');
+      link.href = e.link;
+      link.appendChild(document.createTextNode(e.title));
+      item.appendChild(link);
 
-    list.appendChild(item);
+      list.appendChild(item);
+    });
+
+    return list;
+  };
+
+  jQuery.getFeed({
+    url: 'https://blog.normmaclennan.com/rss/',
+    success: function(feed) {
+      document.getElementById('blog-items').appendChild(makeUL(feed.items));
+      var placeholder = document.getElementById('blog-placeholder');
+      placeholder.parentNode.removeChild(placeholder);
+    }
   });
-
-  return list;
-}
-
-jQuery.getFeed({
-  url: 'https://blog.normmaclennan.com/rss/',
-  success: function(feed) {
-    document.getElementById('blog-items').appendChild(makeUL(feed.items));
-    var placeholder = document.getElementById('blog-placeholder');
-    placeholder.parentNode.removeChild(placeholder);
-  }
 });
